54047536891c7036a57592e6b0c4fd819af92747
David Suarez <david.suarez@intelygenz.com>
2016-01-08 14:36:14 +0100

    First Commit

---
+++ index.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var app = express();
+var redis  = require('redis');
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+
+io.on('connection', function(socket){
+    console.log('Connected...');
+});
+
+var client2 = redis.createClient();
+client2.subscribe("big_data_sim_channel");
+client2.on("message", function(channel, message){
+    console.log('Sending message..', JSON.parse(message));
+    io.emit('events', message);
+});
+
+app.use(express.static('public'));
+
+server.listen(3005);
